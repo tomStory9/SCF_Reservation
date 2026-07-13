d09e489986c73bfa52156947c41337061701df49
Diodoru <newlolo510@gmail.com>
2026-07-13 10:36:59 +0200

    suppression des entités

--- src/Entity/Location.php
+++ src/Entity/Location.php
@@ -3,8 +3,6 @@
 namespace App\Entity;
 
 use App\Repository\LocationRepository;
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: LocationRepository::class)]
@@ -21,15 +19,8 @@ class Location
     #[ORM\Column(length: 255)]
     private ?string $typeLocation = null;
 
-    /**
-     * @var Collection<int, PriceByType>
-     */
-    #[ORM\OneToMany(targetEntity: PriceByType::class, mappedBy: 'location')]
-    private Collection $priceByTypes;
-
     public function __construct()
     {
-        $this->priceByTypes = new ArrayCollection();
     }
 
     public function getId(): ?int
@@ -57,36 +48,6 @@ class Location
     public function setTypeLocation(string $typeLocation): static
     {
         $this->typeLocation = $typeLocation;
-
-        return $this;
-    }
-
-    /**
-     * @return Collection<int, PriceByType>
-     */
-    public function getPriceByTypes(): Collection
-    {
-        return $this->priceByTypes;
-    }
-
-    public function addPriceByType(PriceByType $priceByType): static
-    {
-        if (!$this->priceByTypes->contains($priceByType)) {
-            $this->priceByTypes->add($priceByType);
-            $priceByType->setLocation($this);
-        }
-
-        return $this;
-    }
-
-    public function removePriceByType(PriceByType $priceByType): static
-    {
-        if ($this->priceByTypes->removeElement($priceByType)) {
-            // set the owning side to null (unless already changed)
-            if ($priceByType->getLocation() === $this) {
-                $priceByType->setLocation(null);
-            }
-        }
 
         return $this;
     }
