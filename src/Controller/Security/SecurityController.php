b5494ab60c829409828e8d8d563d9cdbdd1900b7
Tom raineri <tomstory9@gmail.com>
2026-07-15 08:29:29 +0000

    error when mail not validated

--- src/Controller/Security/SecurityController.php
+++ src/Controller/Security/SecurityController.php
@@ -16,30 +16,11 @@ class SecurityController extends AbstractController
     #[Route(path: '/login', name: 'app_login')]
     public function login(AuthenticationUtils $authenticationUtils, TranslatorInterface $translator, Request $request, EntityManagerInterface $entityManager): Response
     {
-        if ($this->getUser() and $this->getUser()->isVerified() and !$this->getUser()->isFilledInfo()) {
-            return $this->redirectToRoute('app_register_information');
-        } elseif ($this->getUser()) {
-            return $this->redirectToRoute('app_home');
-        }
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
 
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
-
-        if ($request->isMethod('POST')) {
-            $email = $request->request->get('email');
-            $user = $entityManager->getRepository(User::class)->findOneBy(['email' => $email]);
-
-            if ($user && !$user->isVerified()) {
-                $this->addFlash('warning', $translator->trans('flash.error_not_verified', domain: 'validators'));
-
-                return $this->render('security/CandidateRegistration/candidate_login.html.twig', [
-                    'last_username' => $email,
-                    'error' => null,
-                ]);
-            }
-        }
 
         return $this->render('security/login/index.html.twig', [
             'last_username' => $lastUsername,
---
+++ src/Security/UserChecker.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Security;
+
+use App\Entity\User as AppUser;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Exception\CustomUserMessageAccountStatusException;
+use Symfony\Component\Security\Core\User\UserCheckerInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Contracts\Translation\TranslatorInterface;
+
+class UserChecker implements UserCheckerInterface
+{
+    public function __construct(private TranslatorInterface $translator)
+    {
+    }
+
+    public function checkPreAuth(UserInterface $user): void
+    {
+        if (!$user instanceof AppUser) {
+            return;
+        }
+
+        if (!$user->isVerified()) {
+            // the message passed to this exception is meant to be displayed to the user
+            throw new CustomUserMessageAccountStatusException($this->translator->trans('flash.error_not_verified', domain: 'validators'));
+        }
+    }
+
+    public function checkPostAuth(UserInterface $user, ?TokenInterface $token = null): void
+    {
+    }
+}
