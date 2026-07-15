3c44139019fdb2f787fac7a59191af6c5428c86c
Diodoru <newlolo510@gmail.com>
2026-07-15 09:03:57 +0200

    Ajout tarif reduit A et B dans le pricing

--- src/Entity/Pricing.php
+++ src/Entity/Pricing.php
@@ -21,11 +21,17 @@ class Pricing
     private ?TimeSlot $timeSlot = null;
 
     #[ORM\Column]
-    private ?int $price = null;
+    private ?int $fullPrice = null;
 
     #[ORM\ManyToOne(inversedBy: 'pricings')]
     #[ORM\JoinColumn(nullable: false)]
     private ?Location $location = null;
+
+    #[ORM\Column]
+    private ?int $reducedPriceA = null;
+
+    #[ORM\Column]
+    private ?int $reducedPriceB = null;
 
     public function getId(): ?int
     {
@@ -56,14 +62,14 @@ class Pricing
         return $this;
     }
 
-    public function getPrice(): ?int
+    public function getFullPrice(): ?int
     {
-        return $this->price;
+        return $this->fullPrice;
     }
 
-    public function setPrice(int $price): static
+    public function setFullPrice(int $fullPrice): static
     {
-        $this->price = $price;
+        $this->fullPrice = $fullPrice;
 
         return $this;
     }
@@ -76,6 +82,30 @@ class Pricing
     public function setLocation(?Location $location): static
     {
         $this->location = $location;
+
+        return $this;
+    }
+
+    public function getReducedPriceA(): ?int
+    {
+        return $this->reducedPriceA;
+    }
+
+    public function setReducedPriceA(int $reducedPriceA): static
+    {
+        $this->reducedPriceA = $reducedPriceA;
+
+        return $this;
+    }
+
+    public function getReducedPriceB(): ?int
+    {
+        return $this->reducedPriceB;
+    }
+
+    public function setReducedPriceB(int $reducedPriceB): static
+    {
+        $this->reducedPriceB = $reducedPriceB;
 
         return $this;
     }
