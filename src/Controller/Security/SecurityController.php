f82b3f49ff8e12c2f62dd443dcae5341e6b740df
Tom raineri <tomstory9@gmail.com>
2026-07-15 06:15:13 +0000

    good error display for login invalid credentials , when mail is not verified and good redirect

--- src/Controller/Security/SecurityController.php
+++ src/Controller/Security/SecurityController.php
@@ -6,19 +6,44 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Contracts\Translation\TranslatorInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\User;
 
 class SecurityController extends AbstractController
+
 {
     #[Route(path: '/login', name: 'app_login')]
-    public function login(AuthenticationUtils $authenticationUtils): Response
-    {
+    public function login(AuthenticationUtils $authenticationUtils , TranslatorInterface $translator, Request $request, EntityManagerInterface $entityManager): Response
+    {   
+        if ($this->getUser() and $this->getUser()->isVerified() and !$this->getUser()->isFilledInfo()) {
+            return $this->redirectToRoute('app_register_information');
+        }
+        else if ($this->getUser()) {
+            return $this->redirectToRoute('app_home');
+        }
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
 
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
-        return $this->render('security/login/index.html.twig', [
+    
+        
+        if ($request->isMethod('POST')) {
+            $email = $request->request->get('email');
+            $user = $entityManager->getRepository(User::class)->findOneBy(['email' => $email]);
+
+            if ($user && !$user->isVerified()) {
+                $this->addFlash('warning', $translator->trans('flash.error_not_verified', domain: 'validators'));
+                return $this->render('security/CandidateRegistration/candidate_login.html.twig', [
+                    'last_username' => $email,
+                    'error' => null,
+                ]);
+            }    
+    }
+       return $this->render('security/login/index.html.twig', [
             'last_username' => $lastUsername,
             'error' => $error,
         ]);
