359ad1525e97e1bb9fcf4c75f35427b54bb6933e
Diodoru <newlolo510@gmail.com>
2026-07-15 10:23:56 +0200

    fixtures User

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class UserFixtures extends Fixture
+{
+    public const string ADMIN = 'admin';
+    public const string CA_USER = 'ca_user';
+    public const string AA_USER = 'aa_user';
+    public const string FA_USER = 'fa_user';
+    public const string TM_USER = 'tm_user';
+    public const string DEFAULT_USER = 'default_user';
+
+    public const string DEFAULT_PASSWORD = 'test';
+
+    public function load(ObjectManager $manager): void
+    {
+        $admin = new User();
+        $admin->setName('SCF_ADMIN');
+        $admin->setEmail('admin@test.test');
+        $admin->setPassword(password_hash('password', self::DEFAULT_PASSWORD));
+        $admin->setRoles(['ROLE_ADMIN']);
+        $manager->persist($admin);
+        $this->addReference(self::ADMIN, $admin);
+
+        $ca_user = new User();
+        $ca_user->setName('Loevann');
+        $ca_user->setLastname('Guegan');
+        $ca_user->setEmail('lguegan@test.test');
+        $ca_user->setPassword(password_hash('password', self::DEFAULT_PASSWORD));
+        $ca_user->setRoles(['ROLE_CA_USER']);
+        $manager->persist($ca_user);
+        $this->addReference(self::CA_USER, $ca_user);
+
+        $aa_user = new User();
+        $aa_user->setName('Tom');
+        $aa_user->setLastname('Raineri');
+        $aa_user->setEmail('traineri@test.test');
+        $aa_user->setPassword(password_hash('password', self::DEFAULT_PASSWORD));
+        $aa_user->setRoles(['ROLE_AA_USER']);
+        $manager->persist($aa_user);
+        $this->addReference(self::AA_USER, $aa_user);
+
+        $fa_user = new User();
+        $fa_user->setName('Simon');
+        $fa_user->setLastname('Ledoux');
+        $fa_user->setEmail('sledoux@test.test');
+        $fa_user->setPassword(password_hash('password', self::DEFAULT_PASSWORD));
+        $fa_user->setRoles(['ROLE_FA_USER']);
+        $manager->persist($fa_user);
+        $this->addReference(self::FA_USER, $fa_user);
+
+        $tm_user = new User();
+        $tm_user->setName('Cacary');
+        $tm_user->setLastname('Riendenin');
+        $tm_user->setEmail('criendenin@test.test');
+        $tm_user->setPassword(password_hash('password', self::DEFAULT_PASSWORD));
+        $tm_user->setRoles(['ROLE_TM_USER']);
+        $manager->persist($tm_user);
+        $this->addReference(self::TM_USER, $tm_user);
+
+        $default_user = new User();
+        $default_user->setName('Grobin');
+        $default_user->setLastname('Ciboulette');
+        $default_user->setEmail('gciboulette@test.test');
+        $default_user->setPassword(password_hash('password', self::DEFAULT_PASSWORD));
+        $default_user->setRoles(['ROLE_DEFAULT_USER']);
+        $manager->persist($default_user);
+        $this->addReference(self::DEFAULT_USER, $default_user);
+
+        $manager->flush();
+    }
+}
