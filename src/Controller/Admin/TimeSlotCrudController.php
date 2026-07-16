2cd280056cdc3a9da135cce88a86a196252df01c
Diodoru <newlolo510@gmail.com>
2026-07-16 09:31:42 +0200

    ajout de l'enum dans le controller easyadmin de timeslot

--- src/Controller/Admin/TimeSlotCrudController.php
+++ src/Controller/Admin/TimeSlotCrudController.php
@@ -3,10 +3,12 @@
 namespace App\Controller\Admin;
 
 use App\Entity\TimeSlot;
+use App\Enum\TimeSlotPeriod;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TimeField;
+use Symfony\Component\Form\Extension\Core\Type\EnumType;
 
 class TimeSlotCrudController extends AbstractCrudController
 {
@@ -15,14 +17,16 @@ class TimeSlotCrudController extends AbstractCrudController
         return TimeSlot::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
-        return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
-        ];
+        yield IdField::new('id');
+        yield TimeField::new('startTime');
+        yield TimeField::new('endTime');
+        yield ChoiceField::new('period', 'Period')
+            ->setFormType(EnumType::class)
+            ->setFormTypeOptions([
+                'class' => TimeSlotPeriod::class,
+            ])
+            ->formatValue(fn ($value, $entity) => $value?->value);
     }
-    */
 }
