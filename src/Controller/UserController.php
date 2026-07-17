510a3b55d2ca4fba4d13642ef3a4c617445d8186
Tom Raineri <tomstory9@gmail.com>
2026-07-17 15:33:22 +0000

    working design ahou ahou

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,7 +2,6 @@
 
 namespace App\Controller;
 
-use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -10,20 +9,19 @@ use Symfony\Component\Routing\Attribute\Route;
 
 final class UserController extends AbstractController
 {
-    public function __construct(private EntityManagerInterface $entityManager)
-    {
+    public function __construct(
+        private EntityManagerInterface $entityManager
+    ) {
     }
 
     #[Route('/user', name: 'app_home')]
     public function index(): Response
     {
         $user = $this->getUser();
-        $bookings = $this->entityManager->getRepository(User::class)->findBy(['userBooking' => $user]);
 
-        return
-        $this->render('user/index.html.twig', [
-            '$user' => $user,
-            'bookings' => $bookings,
+        return $this->render('user/index.html.twig', [
+            'user' => $user,
+            'bookings' => $user->getBookings(),
         ]);
     }
 }
