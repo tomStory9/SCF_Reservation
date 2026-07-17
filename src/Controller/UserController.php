eacef60d02d029e814c8fff3c7b65fea4250a762
Tom raineri <tomstory9@gmail.com>
2026-07-17 05:07:43 +0000

    j'ai mon pc je switch

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,17 +2,28 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class UserController extends AbstractController
 {
+    public function __construct(private EntityManagerInterface $entityManager)
+    {
+    }
+
     #[Route('/user', name: 'app_home')]
     public function index(): Response
     {
-        return $this->render('user/index.html.twig', [
-            'controller_name' => 'UserController',
+        $user = $this->getUser();
+        $bookings = $this->entityManager->getRepository(User::class)->findBy(['userBooking' => $user]);
+
+        return
+        $this->render('user/index.html.twig', [
+            '$user' => $user,
+            'bookings' => $bookings,
         ]);
     }
 }
