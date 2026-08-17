10ea8687095d7fc7f1e2193abb5c016fbccbd48a
Diodoru <newlolo510@gmail.com>
2026-08-17 22:20:42 +0200

    affichage des informations

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,18 +2,18 @@
 
 namespace App\Controller;
 
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 final class UserController extends AbstractController
 {
-    public function __construct(
-        private EntityManagerInterface $entityManager
-    ) {
+    public function __construct()
+    {
     }
 
+    #[IsGranted('ROLE_USER')]
     #[Route('/user', name: 'app_home_user')]
     public function index(): Response
     {
@@ -22,6 +22,17 @@ final class UserController extends AbstractController
         return $this->render('user/index.html.twig', [
             'user' => $user,
             'bookings' => $user->getBookings(),
+        ]);
+    }
+
+    #[IsGranted('ROLE_USER')]
+    #[Route('/profil', name: 'app_user_profile')]
+    public function profile(): Response
+    {
+        $user = $this->getUser();
+
+        return $this->render('user/informations.html.twig', [
+            'user' => $user,
         ]);
     }
 }
