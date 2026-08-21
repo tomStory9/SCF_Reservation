dbb85445514aa1939df463ff36aca77b8479c31c
Tom Raineri <tomstory9@gmail.com>
2026-08-21 07:09:25 +0000

    Feat : New settings

--- src/Entity/Settings.php
+++ src/Entity/Settings.php
@@ -19,6 +19,21 @@ class Settings
     #[ORM\Column]
     private ?bool $isUserValidationRequired = null;
 
+    #[ORM\Column]
+    private ?int $minDayBooking = null;
+
+    #[ORM\Column]
+    private ?int $minDayRoomBooking = null;
+
+    #[ORM\Column]
+    private ?bool $IsPendingBookingBlocking = null;
+
+    #[ORM\Column]
+    private ?int $HourCheckInRoom = null;
+
+    #[ORM\Column]
+    private ?int $HourCheckOut = null;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -44,6 +59,54 @@ class Settings
     public function setIsUserValidationRequired(bool $isUserValidationRequired): static
     {
         $this->isUserValidationRequired = $isUserValidationRequired;
+
+        return $this;
+    }
+
+    public function getMinDayRoomBooking(): ?int
+    {
+        return $this->minDayRoomBooking;
+    }
+
+    public function setMinDayRoomBooking(int $minDayRoomBooking): static
+    {
+        $this->minDayRoomBooking = $minDayRoomBooking;
+
+        return $this;
+    }
+
+    public function isPendingBookingBlocking(): ?bool
+    {
+        return $this->IsPendingBookingBlocking;
+    }
+
+    public function setIsPendingBookingBlocking(bool $IsPendingBookingBlocking): static
+    {
+        $this->IsPendingBookingBlocking = $IsPendingBookingBlocking;
+
+        return $this;
+    }
+
+    public function getHourCheckInRoom(): ?int
+    {
+        return $this->HourCheckInRoom;
+    }
+
+    public function setHourCheckInRoom(int $HourCheckInRoom): static
+    {
+        $this->HourCheckInRoom = $HourCheckInRoom;
+
+        return $this;
+    }
+
+    public function getHourCheckOut(): ?int
+    {
+        return $this->HourCheckOut;
+    }
+
+    public function setHourCheckOut(int $HourCheckOut): static
+    {
+        $this->HourCheckOut = $HourCheckOut;
 
         return $this;
     }
