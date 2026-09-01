f96ffe95db2132bbf9f5707cf2956f8b3b536708
Diodoru <newlolo510@gmail.com>
2026-09-01 08:54:25 +0200

    filter facility

--- src/Controller/Admin/FacilityCrudController.php
+++ src/Controller/Admin/FacilityCrudController.php
@@ -4,8 +4,10 @@ namespace App\Controller\Admin;
 
 use App\Entity\Facility;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Filters;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Filter\TextFilter;
 
 class FacilityCrudController extends AbstractCrudController
 {
@@ -19,6 +21,15 @@ class FacilityCrudController extends AbstractCrudController
         yield TextField::new('name', 'admin.field.name');
         yield TextField::new('address', 'admin.field.address');
         yield TextField::new('internationalAddress', 'admin.field.internationalAddress');
+    }
+
+    public function configureFilters(Filters $filters): Filters
+    {
+        return $filters
+            ->add(TextFilter::new('name', 'admin.field.name'))
+            ->add(TextFilter::new('address', 'admin.field.address'))
+            ->add(TextFilter::new('internationalAddress', 'admin.field.internationalAddress'))
+        ;
     }
 
     public function configureCrud(Crud $crud): Crud
